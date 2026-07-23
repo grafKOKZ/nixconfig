2f2e17b4ced4dab9a74de2044b45251a09546c7f
Klaas <klaas.remmers@proton.me>
2026-07-23 13:34:15 +0200

    openSSH auf server

--- modules/hostnameS.nix
+++ modules/hostnameS.nix
@@ -5,8 +5,19 @@
   
   environment.systemPackages = with pkgs; [
     jdk25
+    tmux
   ];
   networking.firewall = {
-    allowedTCPPorts = [ 28472 ];
+    allowedTCPPorts = [ 28472 48222 ];
+  };
+  services.openssh = {
+    enable = true;
+    ports = [ 48222 ];
+    settings = {
+      PubkeyAuthentication = true;
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
   };
 }
