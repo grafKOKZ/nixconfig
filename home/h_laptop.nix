388a2e0db68172d37ccfdf122a4527dd3858276f
klaas <klaas.remmers@proton.me>
2026-07-29 20:28:52 +0200

    laptop fixes

--- home/h_laptop.nix
+++ home/h_laptop.nix
@@ -7,10 +7,10 @@
     stateVersion = "26.05";
 
     file.".config/kitty/kitty.conf".source = ./1080dots/kitty/kitty.conf;
-    file.".config/hypr/hyprland.lua".source = ./1080dots/hypr/hyprland.lua;
-    file.".config/hypr/hyprpaper.conf".source = ./1080dots/hypr/hyprpaper.conf;
-    file.".config/waybar/config.jsonc".source = ./1080dots/waybar/config.jsonc;
-    file.".config/waybar/style.css".source = ./1080dots/waybar/style.css;
+    file.".config/hypr/hyprland.lua".source = ./1080dots/hyprL/hyprland.lua;
+    file.".config/hypr/hyprpaper.conf".source = ./1080dots/hyprL/hyprpaper.conf;
+    file.".config/waybar/config.jsonc".source = ./1080dots/waybarL/config.jsonc;
+    file.".config/waybar/style.css".source = ./1080dots/waybarL/style.css;
     file.".config/fastfetch/config.jsonc".source = ./1080dots/fastfetch/config.jsonc;
     file.".config/rofi/config.rasi".source = ./1080dots/rofi/config.rasi;
     file.".config/swaync/config.json".source = ./1080dots/swaync/config.json;
